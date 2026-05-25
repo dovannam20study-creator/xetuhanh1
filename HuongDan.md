e101b9d95036a5e8c64fcfd98ea8a73950b42b93
Do Van Nam <dovannam.20.study@gmail.com>
2026-05-25 04:50:01 -0400

    3h45 25/5

---
+++ HuongDan.md
@@ -0,0 +1,5 @@
+
+
+
+Buoc 1: Phai khoi chay ROS2 
+Buoc 2: Vao khong gian wordspace
--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -9,19 +9,25 @@ from launch_ros.actions import Node
 
 import xacro
 
-
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Lấy giá trị tham số xem có sử dụng thời gian mô phỏng hay không
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('my_bot'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # Đường dẫn đến file robot.urdf.xacro
+    pkg_path = os.path.join(get_package_share_directory('xetuhanh1'))
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    
+    # Chuyển đổi file xacro thành định dạng xml để robot_state_publisher có thể hiểu
     robot_description_config = xacro.process_file(xacro_file).toxml()
     
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config, 'use_sim_time': use_sim_time}
+    # Thiết lập tham số cho node robot_state_publisher
+    params = {
+        'robot_description': robot_description_config,  
+        'use_sim_time': use_sim_time
+    }
+    
+    # Tạo node robot_state_publisher (node này giúp công bố trạng thái các khớp của robot)
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -29,13 +35,21 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Node này sẽ tạo ra các khớp giả lập để RSP có thể tính toán TF
+    node_joint_state_publisher = Node(
+        package='joint_state_publisher_gui', # Dùng bản GUI để bạn có thể vặn khớp
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+    )
 
-    # Launch!
+    # Trả về danh sách các tác vụ cần chạy khi launch
     return LaunchDescription([
+        # Khai báo đối số (argument) có thể truyền vào khi chạy lệnh ros2 launch
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
+            description='Sử dụng thời gian mô phỏng nếu là true'),
 
+        # Thêm node vào tiến trình khởi chạy
         node_robot_state_publisher
     ])
